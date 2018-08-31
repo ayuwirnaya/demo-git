aac4a0af651c970f3c9fe0ef664cf5d5ad929e8f
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-08-31 16:07:33 +0700

    week 2 exercise-9

---
+++ exercises9.js
@@ -0,0 +1,17 @@
+//1
+function shotOut() {
+  console.log("halo function!");
+}
+
+shotOut();
+
+//2
+var num1 = 5;
+var num2 = 6;
+function calculateMultiply(num1, num2) {
+  return num1 * num2;
+}
+console.log (calculateMultiply(5,6));
+
+
+

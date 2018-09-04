96820c1c02af0307facecfedd1fb23762c57b8f9
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-04 14:50:23 +0700

    exercises9.js week-2

--- exercises9.js
+++ exercises9.js
@@ -13,5 +13,12 @@ function calculateMultiply(num1, num2) {
 }
 console.log (calculateMultiply(5,6));
 
+3//
+
+  var name = 'Agus';
+  var age = '30';
+  var address = 'Jln. Malioboro, Yogjakarta';
+  var hobby = 'gaming';
+  console.log('Nama saya:  '+name+', umur saya: '+age+', alamat saya: '+address+', hobi saya yaitu: '+hobby+'');
 
 

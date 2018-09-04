9f8efd9c1e0b8fc38695946e68c7709677320f01
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-04 14:47:53 +0700

    exercises 11 week-2

---
+++ exercises11.js
@@ -0,0 +1,42 @@
+function balikKata(str) {
+    var array = str.split("");
+    array.reverse();
+    var result = array.join("");
+    return result;
+    }
+    console.log(balikKata("Hello World and Coders"));
+
+function balikKata(str) {
+        var array = str.split("");
+        array.reverse();
+        var result = array.join("");
+        return result;
+        }
+    console.log(balikKata('John Doe')); 
+
+function balikKata(str) {
+        var array = str.split("");
+        array.reverse();
+        var result = array.join("");
+        return result;
+        }
+        console.log(balikKata('I am a bookworm'));
+
+function balikKata(str) {
+    var array = str.split("");
+    array.reverse();
+    var result = array.join("");
+    return result;
+    }
+    console.log(balikKata('Coding is my hobby'));
+
+function balikKata(str) {
+    var array = str.split("");
+    array.reverse();
+    var result = array.join("");
+    return result;
+    }
+    console.log(balikKata('Super'));
+    
+ 
+  

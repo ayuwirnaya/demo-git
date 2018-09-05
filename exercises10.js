e99bb9d3875cd8a3389529fc50ff69a14dc8820f
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-05 11:15:29 +0700

    exercise 10 week-2

---
+++ exercises10.js
@@ -0,0 +1,25 @@
+function bandingkanAngka(angka1, angka2) {
+    return angka1 < angka2
+  }
+  console.log(bandingkanAngka(5, 8)); // true
+
+  function bandingkanAngka(angka1, angka2) {
+    return angka1 < angka2
+  } 
+  console.log(bandingkanAngka(5, 3)); // false
+  
+  function bandingkanAngka(angka1, angka2) {
+    return angka1 = angka2
+  }
+    console.log(bandingkanAngka(4, 4)); // -1
+
+  function bandingkanAngka(angka1, angka2) {
+    return angka1 = angka2
+  }
+    console.log(bandingkanAngka(3, 3)); // -1
+  
+  function bandingkanAngka(angka1, angka2) {
+        return angka1 < angka2
+  } 
+    console.log(bandingkanAngka(17, 2)); // false
+  

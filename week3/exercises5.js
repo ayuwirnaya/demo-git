ea20e993dfcd7aa4e839b29d6cc46276b95e5f04
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-06 00:54:09 +0700

    Add files via upload

---
+++ week3/exercises5.js
@@ -0,0 +1,62 @@
+function palindrome(str) {
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+  }
+console.log (palindrome("katak"));
+
+//
+
+function palindrome(str) {
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+  }
+console.log (palindrome("blanket"));
+
+//
+
+function palindrome(str) {
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+  }
+console.log (palindrome("civic"));
+
+//
+
+function palindrome(str) {
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+  }
+console.log (palindrome("kasur rusak"));
+
+//
+
+function palindrome(str) {
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+  }
+console.log (palindrome("mister"));

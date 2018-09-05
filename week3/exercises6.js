f7c71a6b1dda4c69172c4d9d1c7907e09842a338
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-06 00:55:28 +0700

    Add files via upload

---
+++ week3/exercises6.js
@@ -0,0 +1,67 @@
+function angkaPalindrome(num) {
+    while (true) {
+      num++;
+      var str = String(num);
+      var strReverse = str.split('').reverse().join('');
+      if (str === strReverse) {
+        return Number(str);
+      }
+    }
+  }
+    console.log(angkaPalindrome(8));
+
+    //
+
+    function angkaPalindrome(num) {
+        while (true) {
+          num++;
+          var str = String(num);
+          var strReverse = str.split('').reverse().join('');
+          if (str === strReverse) {
+            return Number(str);
+          }
+        }
+      }
+        console.log(angkaPalindrome(10));
+
+        //
+
+        function angkaPalindrome(num) {
+            while (true) {
+              num++;
+              var str = String(num);
+              var strReverse = str.split('').reverse().join('');
+              if (str === strReverse) {
+                return Number(str);
+              }
+            }
+          }
+            console.log(angkaPalindrome(117));
+
+            //
+
+            function angkaPalindrome(num) {
+                while (true) {
+                  num++;
+                  var str = String(num);
+                  var strReverse = str.split('').reverse().join('');
+                  if (str === strReverse) {
+                    return Number(str);
+                  }
+                }
+              }
+                console.log(angkaPalindrome(175));
+
+                //
+
+                function angkaPalindrome(num) {
+                    while (true) {
+                      num++;
+                      var str = String(num);
+                      var strReverse = str.split('').reverse().join('');
+                      if (str === strReverse) {
+                        return Number(str);
+                      }
+                    }
+                  }
+                    console.log(angkaPalindrome(1000));

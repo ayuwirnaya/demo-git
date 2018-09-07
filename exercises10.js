166562a7cbfa77a2b747f31790a2056de0e6dba0
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-07 19:05:38 +0700

    Update exercises10.js

--- exercises10.js
+++ exercises10.js
@@ -1,25 +1,56 @@
+var angka1 = 5;
+var angka2 = 8;
 function bandingkanAngka(angka1, angka2) {
-    return angka1 < angka2
+  if (angka1 < angka2) {
+    return true;
+  } else {
+    return false;
   }
-  console.log(bandingkanAngka(5, 8)); // true
+}
+console.log(bandingkanAngka(5, 8)); // true
 
-  function bandingkanAngka(angka1, angka2) {
-    return angka1 < angka2
-  } 
-  console.log(bandingkanAngka(5, 3)); // false
-  
-  function bandingkanAngka(angka1, angka2) {
-    return angka1 = angka2
+var angka1 = 5;
+var angka2 = 3;
+function bandingkanAngka(angka1, angka2) {
+  if (angka1 > angka2) {
+    return true;
+  } else {
+    return false;
   }
-    console.log(bandingkanAngka(4, 4)); // -1
+}
+console.log(bandingkanAngka(5, 3)); // false
 
-  function bandingkanAngka(angka1, angka2) {
-    return angka1 = angka2
+var angka1 = 4;
+var angka2 = 4;
+function bandingkanAngka(angka1, angka2) {
+  if (angka1 === angka2) {
+    return true;
+  } else {
+    return -1;
   }
-    console.log(bandingkanAngka(3, 3)); // -1
-  
-  function bandingkanAngka(angka1, angka2) {
-        return angka1 < angka2
-  } 
-    console.log(bandingkanAngka(17, 2)); // false
+}
+console.log(bandingkanAngka(4, 4)); // -1
+
+var angka1 = 3;
+var angka2 = 3;
+function bandingkanAngka(angka1, angka2) {
+  if (angka1 !== angka2) {
+    return false;
+  } else {
+    return -1;
+  }
+}
+console.log(bandingkanAngka(3, 3)); // -1
+
+var angka1 = 17;
+var angka2 = 2;
+function bandingkanAngka(angka1, angka2) {
+  if (angka1 > angka2) {
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log(bandingkanAngka(17, 2)); // false
+
   

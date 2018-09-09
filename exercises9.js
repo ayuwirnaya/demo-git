8f591bbc245cfcee7547c642adbf893b5e0a671f
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-10 00:57:14 +0700

    Update exercises9.js

--- exercises9.js
+++ exercises9.js
@@ -15,10 +15,16 @@ console.log (calculateMultiply(5,6));
 
 3//
 
-  var name = 'Agus';
-  var age = '30';
-  var address = 'Jln. Malioboro, Yogjakarta';
-  var hobby = 'gaming';
-  console.log('Nama saya:  '+name+', umur saya: '+age+', alamat saya: '+address+', hobi saya yaitu: '+hobby+'');
+
+  var fullSentence = processSentence(name,age,address,hobby)
+  var name = 'Agus,';
+  var age = '30,';
+  var address = 'Jln. Malioboro, Yogjakarta,';
+  var hobby = 'gaming,';
+  function processSentence() {
+    return fullSentence + processSentence;
+    }
+
+console.log('Nama saya '  +name, 'Usia saya '  +age, 'Alamat saya di ' +address, 'Hobby saya ' +hobby,);
 
 

2cc35bb40b4b2eab562f7f5eacebf6fee63aef4e
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-16 10:08:31 +0800

    Add files via upload

---
+++ week3/exercises14.js
@@ -0,0 +1,27 @@
+function mengelompokkanAngka(arr) {
+  var genap = []
+  var ganjil = []
+  var kelipatantiga = []
+  var result = []
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0 && arr[i] % 3 !== 0) {
+      genap.push(arr[i]);
+    } else if (arr[i] % 2 !== 0 && arr[i] % 3 !== 0) {
+      ganjil.push (arr[i]);
+    } else if (arr[i] % 3 === 0) {
+      kelipatantiga.push (arr[i]);
+    }
+  }
+  result.push(genap);
+  result.push(ganjil);
+  result.push(kelipatantiga);
+
+  return result;
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+  console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+  console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+  console.log(mengelompokkanAngka([])); // [ [], [], [] ]

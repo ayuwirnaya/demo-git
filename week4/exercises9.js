8ae4644237ff6ef646f32ee75d071b34f30b81fd
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-16 15:10:54 +0800

    Add files via upload

---
+++ week4/exercises9.js
@@ -0,0 +1,21 @@
+
+// Diberikan function tukarBesarKecil(kalimat) yang menerima satu parameter berupa string. 
+// Function akan me-return string tersebut dengan menukar karakter yang uppercase menjadi 
+// lowercase, dan sebaliknya. Spasi dan simbol diabaikan.
+
+function tukarBesarKecil(kalimat) {
+    var hasil = '';
+
+    for(var i = 0; i < kalimat.length; i++){
+        hasil += kalimat[i] !== kalimat[i].toLowerCase() ? 
+        kalimat[i].toLowerCase() : kalimat[i].toUpperCase()
+    }
+    return hasil;
+  }
+  
+  // TEST CASES
+  console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+  console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+  console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+  console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+  console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"

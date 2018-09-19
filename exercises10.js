2d8f8395a159631b3261ccb579b163a5da507367
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-19 23:42:03 +0700

    bandingkan angka

--- exercises10.js
+++ exercises10.js
@@ -1,56 +1,19 @@
-var angka1 = 5;
-var angka2 = 8;
 function bandingkanAngka(angka1, angka2) {
-  if (angka1 < angka2) {
+  // you can only write your code here!
+  if (angka2 > angka1) {
     return true;
+  } else if (angka1 === angka2) {
+    return -1;
   } else {
     return false;
   }
 }
+
+// TEST CASES
 console.log(bandingkanAngka(5, 8)); // true
-
-var angka1 = 5;
-var angka2 = 3;
-function bandingkanAngka(angka1, angka2) {
-  if (angka1 > angka2) {
-    return true;
-  } else {
-    return false;
-  }
-}
 console.log(bandingkanAngka(5, 3)); // false
-
-var angka1 = 4;
-var angka2 = 4;
-function bandingkanAngka(angka1, angka2) {
-  if (angka1 === angka2) {
-    return true;
-  } else {
-    return -1;
-  }
-}
 console.log(bandingkanAngka(4, 4)); // -1
-
-var angka1 = 3;
-var angka2 = 3;
-function bandingkanAngka(angka1, angka2) {
-  if (angka1 !== angka2) {
-    return false;
-  } else {
-    return -1;
-  }
-}
 console.log(bandingkanAngka(3, 3)); // -1
-
-var angka1 = 17;
-var angka2 = 2;
-function bandingkanAngka(angka1, angka2) {
-  if (angka1 > angka2) {
-    return false;
-  } else {
-    return true;
-  }
-}
 console.log(bandingkanAngka(17, 2)); // false
 
   

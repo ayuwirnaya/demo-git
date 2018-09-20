86ef087066da070617a1e4c7e823bc9ac8e44748
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-20 12:07:42 +0700

    hitung huruf

--- week4/exercises6.js
+++ week4/exercises6.js
@@ -1,28 +1,67 @@
-// //
-// Diberikan function hitungHuruf(kalimat) yang akan menerima satu parameter berupa string.
-//  Function akan me-return kata dari kalimat yang memiliki jumlah perulangan huruf yang paling 
-//  besar. Contoh: "Today, is the greatest day ever!" akan me-return "greatest" karena kata
-//   "greatest" memiliki 2 huruf e dan 2 huruf t, dan muncul lebih dulu. Jika tidak ditemukan 
-//   kalimat dengan kata yang memiliki huruf berulang, return -1.
-
-function hitungHuruf(str) {
-    var isSame = false
-    var arrWord = []
-
-    var b = str.split(' ')
-    for (var i = 0; i < b.length; i++){
-        for(var a = 0; a < b[i].length; a++){
-            console.log(b[i])
-            // if (b[i] === b[a]) {
-
-            // }
+function angkaPalindrome(num) {
+    while (true) {
+        num++;
+        var str = String(num);
+        var strReverse = str.split('').reverse().join('');
+        if (str === strReverse) {
+            return Number(str);
         }
     }
-  }
-  
-  // TEST CASES
-  console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
-//   console.log(hitungHuruf('I am a passionate developer')); // passionate
-//   console.log(hitungHuruf('aku adalah anak gembala')); // adalah
-//   console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
-//   console.log(hitungHuruf('mengayuh perahu di danau')); // danau
+}
+console.log(angkaPalindrome(8));
+
+//
+
+function angkaPalindrome(num) {
+    while (true) {
+        num++;
+        var str = String(num);
+        var strReverse = str.split('').reverse().join('');
+        if (str === strReverse) {
+            return Number(str);
+        }
+    }
+}
+console.log(angkaPalindrome(10));
+
+//
+
+function angkaPalindrome(num) {
+    while (true) {
+        num++;
+        var str = String(num);
+        var strReverse = str.split('').reverse().join('');
+        if (str === strReverse) {
+            return Number(str);
+        }
+    }
+}
+console.log(angkaPalindrome(117));
+
+//
+
+function angkaPalindrome(num) {
+    while (true) {
+        num++;
+        var str = String(num);
+        var strReverse = str.split('').reverse().join('');
+        if (str === strReverse) {
+            return Number(str);
+        }
+    }
+}
+console.log(angkaPalindrome(175));
+
+//
+
+function angkaPalindrome(num) {
+    while (true) {
+        num++;
+        var str = String(num);
+        var strReverse = str.split('').reverse().join('');
+        if (str === strReverse) {
+            return Number(str);
+        }
+    }
+}
+console.log(angkaPalindrome(1000));

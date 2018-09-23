050279fd8edb9221d5e8befd1175a8df0f0b4a54
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-23 19:21:01 +0700

    Update exercises4.js

--- week4/exercises4.js
+++ week4/exercises4.js
@@ -1,26 +1,38 @@
 function cariModus(arr) {
-  var arrModus = []
-  var angkaSama = false;
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = i + 1; j < arr.length; j++) {
-      if (arr[i] === arr[j]) {
-        angkaSama = true;
-        arrModus.push(arr[i]);
-      } else {
-        angkaSama = false;
+
+var frekuensi = 0;
+var greatestFrekuensi = 0;
+var modus = 0;
+
+for(var i = 0; i < arr.length; i ++) {
+  for(var j = 0; j < arr.length; j ++) {
+    //console.log(arr[i]+ '-' + arr[j]);
+    if(arr[i] === arr[j] && i !== j) {
+      frekuensi ++;
+      if(frekuensi > greatestFrekuensi) {
+        greatestFrekuensi = frekuensi;
+        modus = i;
       }
     }
   }
-
-  if (arrModus[0] === undefined || angkaSama) {
-    return -1
-  } else {
-    return arrModus[0]
-  }
-
 }
 
+if(modus === 0) {
+  return -1;
+}
 
+var angkaSama = 0;
+for(var k = 0; k < arr.length; k ++) {
+  angkaSama += arr[k];
+  if(angkaSama/arr.length === arr[k]) {
+    return -1;
+  }
+}
+return arr[modus];
+  }
+  
+ 
+  
 // TEST CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
 console.log(cariModus([5, 10, 10, 6, 5])); // 5

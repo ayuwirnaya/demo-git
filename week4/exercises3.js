bd73d5bd9b089b66e2f70d2d97044fb50d0d46c2
ayuwirnaya <42540049+ayuwirnaya@users.noreply.github.com>
2018-09-25 10:31:14 +0700

    Update exercises3.js

--- week4/exercises3.js
+++ week4/exercises3.js
@@ -1,27 +1,21 @@
 
-// function cariMedian(arr) {
-//  var median = 0;
-//  for (var i = 0; i < arr.length; i++){
-//      if(arr.length % 2 === 0){
-//          median = (arr[arr.length / 2 - 1] + arr[arr.length / 2]) / 2;
-//      } else {
-//          median = arr[(arr.length - 1)] / 2;
-//      }
-//      return median;
-//  }
-//   }
 function cariMedian(arr) {
-    var median = 0;
-    for (var i = 0; i < arr.length; i++){
-        median += arr[i]
-    }
+    var median = Math.floor(arr.length / 2)
 
-    return Math.floor(median / arr.length);
+    for (var i = 0; i < arr.length; i++) {
+        if (arr.length % 2) {
+            return arr[median];
+        } else {
+            return (arr[median - 1] + arr[median]) / 2;
+        }
+    }
 }
-  
-  //TEST CASES
-  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
-  console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
-  console.log(cariMedian([3, 4, 7, 6, 10])); // 7
-  console.log(cariMedian([1, 3, 3])); // 3
-  console.log(cariMedian([7, 7, 8, 8])); // 7.5
+
+
+
+//TEST CASES
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
